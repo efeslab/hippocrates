6ef0aac308947b3126c3b93e3884985e11980ba2
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-06-17 17:54:48 -0400

    Compile and run simple test using PMTest

--- tests/manual/000_simple_bug.c
+++ tests/manual/000_simple_bug.c
@@ -6,12 +6,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <libpmem.h>
 #include <pmtest.h>
 
+/**
+ * Adapted from PMTest test #1.
+ * https://github.com/sihangliu/pmtest/blob/master/src/test.cc
+ */
+
 int main(int argc, char *argv[]) {
-    int fd = open("/tmp/000_simple_bug.pmem", O_RDWR | O_CREAT, 0666);
-    if (fd < 0) {
-      perror("open");
-    }
+    char arr[100];
+
+	void *p = NULL;
+
+	PMTest_init(p, 2);
+	PMTest_START;
+
+	PMTest_assign((void *)(&arr[0]), 4);
+	PMTest_fence();
+	PMTest_assign((void *)(&arr[4]), 4);
+	PMTest_flush((void *)(&arr[0]), 4);
+	PMTest_flush((void *)(&arr[0]), 4);
+	PMTest_isPersistent((void *)(&arr[0]), 4);
+	PMTest_isPersistent((void *)(&arr[0]), 0);
+	PMTest_isPersistedBefore((void *)(&arr[0]), 4, (void *)(&arr[0]), 4);
+
+	PMTest_sendTrace(p);
+
+	PMTest_END;
+	PMTest_getResult(p);
+	PMTest_exit(p);
+
+	printf("Test complete!\n");
 }
