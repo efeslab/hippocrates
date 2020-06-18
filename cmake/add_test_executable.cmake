7475582fd0c13d3f400e329da69e1f2769fb62ff
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-06-18 15:10:44 -0400

    Get tests to compile with WLLVM for ease of doing LLVM passes

---
+++ cmake/add_test_executable.cmake
@@ -0,0 +1,36 @@
+
+function(add_test_executable)
+    # Need to ensure that we're running with WLLVM as our compiler.
+    # --https://gitlab.kitware.com/cmake/community/-/wikis/FAQ#how-do-i-use-a-different-compiler
+    string(FIND ${CMAKE_C_COMPILER} "wllvm" C_WLLVM)
+    string(FIND ${CMAKE_CXX_COMPILER} "wllvm++" CXX_WLLVM)
+
+    if (${C_WLLVM} EQUAL -1 OR ${CXX_WLLVM} EQUAL -1)
+        message(FATAL_ERROR "Must set wllvm and wllvm++ as C and CXX compiler.")
+    endif()
+
+    # # Ensure environment variables are set.
+    if (NOT DEFINED ENV{LLVM_COMPILER} OR NOT DEFINED ENV{LLVM_COMPILER_PATH})
+        message(FATAL_ERROR "Must set LLVM_COMPILER and LLVM_COMPILER_PATH environment variables")
+    endif()
+
+    # Now that we know that our compiler is in order, we can parse args.
+    set(options)                                                                   
+    set(oneValueArgs TARGET)                                                       
+    set(multiValueArgs SOURCES EXTRA_LIBS)                                         
+    cmake_parse_arguments(FN_ARGS "${options}" "${oneValueArgs}"                   
+                        "${multiValueArgs}" ${ARGN})
+    
+    add_executable(${FN_ARGS_TARGET} ${FN_ARGS_SOURCES})
+    target_include_directories(${FN_ARGS_TARGET} PUBLIC ${PMTEST_INCLUDE})
+    target_link_libraries(${FN_ARGS_TARGET} pmtest pthread)
+    target_compile_options(${FN_ARGS_TARGET} PUBLIC "-g")
+    
+    add_custom_command(TARGET ${FN_ARGS_TARGET}
+                       POST_BUILD
+                       COMMAND extract-bc $<TARGET_FILE:${FN_ARGS_TARGET}>
+                               -o $<TARGET_FILE:${FN_ARGS_TARGET}>.bc
+                       COMMENT "Hello!")
+    
+endfunction()
+
--- tests/manual/000_simple_bug.c
+++ tests/manual/000_simple_bug.c
@@ -13,24 +13,48 @@
  * https://github.com/sihangliu/pmtest/blob/master/src/test.cc
  */
 
-int main(int argc, char *argv[]) {
-    char arr[100];
+void correct(void *p) {
+	char arr[100];
+	PMTest_assign((void *)(&arr[0]), 4);
+	PMTest_flush((void *)(&arr[0]), 4);
+	PMTest_fence();
 
+	PMTest_assign((void *)(&arr[4]), 4);
+	PMTest_flush((void *)(&arr[4]), 4);
+	PMTest_fence();
+
+	PMTest_isPersistent((void *)(&arr[0]), 0);
+	PMTest_isPersistent((void *)(&arr[4]), 4);
+	PMTest_isPersistedBefore((void *)(&arr[0]), 4, (void *)(&arr[4]), 4);
+	PMTest_sendTrace(p);
+}
+
+void incorrect(void *p) {
+	char arr[100];
+	PMTest_assign((void *)(&arr[0]), 4);
+	PMTest_flush((void *)(&arr[0]), 4);
+	PMTest_fence();
+
+	PMTest_assign((void *)(&arr[4]), 4);
+	// PMTest_flush((void *)(&arr[4]), 4);
+	// PMTest_fence();
+
+	PMTest_isPersistent((void *)(&arr[0]), 0);
+	PMTest_isPersistent((void *)(&arr[4]), 4);
+	PMTest_isPersistedBefore((void *)(&arr[0]), 4, (void *)(&arr[4]), 4);
+	PMTest_sendTrace(p);
+}
+
+int main(int argc, char *argv[]) {
 	void *p = NULL;
+
+	printf("Starting testing...\n");
 
 	PMTest_init(p, 2);
 	PMTest_START;
 
-	PMTest_assign((void *)(&arr[0]), 4);
-	PMTest_fence();
-	PMTest_assign((void *)(&arr[4]), 4);
-	PMTest_flush((void *)(&arr[0]), 4);
-	PMTest_flush((void *)(&arr[0]), 4);
-	PMTest_isPersistent((void *)(&arr[0]), 4);
-	PMTest_isPersistent((void *)(&arr[0]), 0);
-	PMTest_isPersistedBefore((void *)(&arr[0]), 4, (void *)(&arr[0]), 4);
-
-	PMTest_sendTrace(p);
+	correct(p);
+	incorrect(p);
 
 	PMTest_END;
 	PMTest_getResult(p);
