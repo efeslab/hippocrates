b97bd24fe78929fd162a70f99a9b1cfc3e9c7d93
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-06-23 12:00:06 -0400

    Make perf microbenchmark better
    - more precise and now exports CSV!

--- tests/perf/000_simple_perf.c
+++ tests/perf/000_simple_perf.c
@@ -21,8 +21,6 @@ void do_gettimeofday(struct timeval *tv) {
         perror("time start");
         exit(-1);
     }
-
-    _mm_mfence();
 }
 
 uint64_t get_diff(struct timeval *start, struct timeval *end) {
@@ -59,22 +57,49 @@ void non_durable(void *pmem, size_t len, size_t nops) {
     }
 }
 
-void perf_bug(void *pmem, size_t len, size_t nops) {
+void extra_flush(void *pmem, size_t len, size_t nops, double ratio) {
     uint8_t *addr_min = (uint8_t*)pmem;
     uint8_t *addr = addr_min;
     uint8_t *addr_max = pmem + len;
     uint8_t val = (uint8_t)rand();
 
+    double count = ratio;
+
     for (size_t i = 0; i < nops; ++i) {
         *addr = (uint8_t)(val + i);
-        _mm_sfence();
         _mm_clflushopt(addr);
+        if (count > 1.0) {
+            _mm_clflushopt(addr);
+            count -= 1.0;
+        }
         _mm_sfence();
         // next
         addr = (addr + 64) >= addr_max ? addr_min : addr + 64;
+        count += ratio;
     }
 }
 
+void extra_fence(void *pmem, size_t len, size_t nops, double ratio) {
+    uint8_t *addr_min = (uint8_t*)pmem;
+    uint8_t *addr = addr_min;
+    uint8_t *addr_max = pmem + len;
+    uint8_t val = (uint8_t)rand();
+
+    double count = ratio;
+
+    for (size_t i = 0; i < nops; ++i) {
+        if (count > 1.0) {
+            _mm_sfence();
+            count -= 1.0;
+        }
+        *addr = (uint8_t)(val + i);
+        _mm_clwb(addr);
+        _mm_sfence();
+        // next
+        addr = (addr + 64) >= addr_max ? addr_min : addr + 64;
+        count += ratio;
+    }
+}
 
 uint64_t runner(void (*kernel)(void*,size_t,size_t), void *pmem, size_t len, size_t nops) {
     struct timeval start, end;
@@ -83,7 +108,27 @@ uint64_t runner(void (*kernel)(void*,size_t,size_t), void *pmem, size_t len, siz
 
     do_gettimeofday(&start);
 
+    _mm_mfence();
     kernel(pmem, len, nops);
+    _mm_mfence();
+
+    do_gettimeofday(&end);
+
+    return get_diff(&start, &end);
+}
+
+
+uint64_t config_runner(void (*kernel)(void*,size_t,size_t,double), 
+                       void *pmem, size_t len, size_t nops, double ratio) {
+    struct timeval start, end;
+
+    pmem_memset_persist(pmem, 0, len);
+
+    do_gettimeofday(&start);
+
+    _mm_mfence();
+    kernel(pmem, len, nops, ratio);
+    _mm_mfence();
 
     do_gettimeofday(&end);
 
@@ -91,8 +136,8 @@ uint64_t runner(void (*kernel)(void*,size_t,size_t), void *pmem, size_t len, siz
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 4) {
-        fprintf(stderr, "Usage: %s <file> <size> <nops>\n", argv[0]);
+	if (argc < 5) {
+        fprintf(stderr, "Usage: %s <file> <size> <nops> <trials>\n", argv[0]);
         return -1;
     }
 
@@ -104,6 +149,7 @@ int main(int argc, char *argv[]) {
     size_t len = atoll(argv[2]);
     size_t mapped_len;
     size_t nops = atoll(argv[3]);
+    size_t ntrials = atoll(argv[4]);
     int is_pmem;
 
 	if ((pmemaddr = pmem_map_file(fname, len, PMEM_FILE_CREATE, 0666,
@@ -120,12 +166,61 @@ int main(int argc, char *argv[]) {
         printf("Warning: Original length: %lu, mapped: %lu\n", len, mapped_len);
     }
 
-    uint64_t usec_correct = runner(correct, pmemaddr, mapped_len, nops);
-    uint64_t usec_non_durable = runner(non_durable, pmemaddr, mapped_len, nops);
-    uint64_t usec_perf_bug = runner(perf_bug, pmemaddr, mapped_len, nops);
+    // Do trials, output to CSV to stdout.
 
-    printf("Correct: %lu usec\nNo Persists: %lu usec\nRedundant Persists: %lu usec\n",
-            usec_correct, usec_non_durable, usec_perf_bug);
+    printf("File Size,Num Ops,Num Trials,Trial Num,"
+           "Correct Total Time (usec),Correct Time/Op (usec),"
+           "Non-Durable Total Time (usec),Non-Durable Time/Op (usec)," 
+           "2X Flushes Total Time (usec),2X Flushes Time/Op (usec),"
+           "1.5X Flushes Total Time (usec),1.5X Flushes Time/Op (usec),"
+           "1.1X Flushes Total Time (usec),1.1X Flushes Time/Op (usec),"
+           "2X Fences Total Time (usec),2X Fences Time/Op (usec),"
+           "1.5X Fences Total Time (usec),1.5X Fences Time/Op (usec),"
+           "1.1X Fences Total Time (usec),1.1X Fences Time/Op (usec)\n");
+
+    for (size_t t = 0; t < ntrials; ++t) {
+        printf("%lu,%lu,%lu,%lu,", mapped_len, nops, ntrials, t);
+        uint64_t usec;
+        double per_op;
+
+#if 1
+        usec = runner(correct, pmemaddr, mapped_len, nops);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+#else
+        usec = config_runner(extra_fence, pmemaddr, mapped_len, nops, 1.0);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+#endif
+        usec = runner(non_durable, pmemaddr, mapped_len, nops);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_flush, pmemaddr, mapped_len, nops, 1.0);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_flush, pmemaddr, mapped_len, nops, 0.5);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_flush, pmemaddr, mapped_len, nops, 0.1);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_fence, pmemaddr, mapped_len, nops, 1.0);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_fence, pmemaddr, mapped_len, nops, 0.5);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f,", usec, per_op);
+
+        usec = config_runner(extra_fence, pmemaddr, mapped_len, nops, 0.1);
+        per_op = (double)usec / (double)nops;
+        printf("%lu,%f\n", usec, per_op);
+    }
+
 
     pmem_unmap(pmemaddr, mapped_len);
 
