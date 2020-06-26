fbe1bd6153be17658d102067247e47931caf264d
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-06-26 16:44:21 -0400

    Boilerplate for pmemcheck
    - Turns out pmemcheck gives us almost everything we want, so it's easy
    to set up and get traces from (although in a different format)
    - It may be worth getting the trace to also output locations for flushes
    and fences, although maybe the bug information would be enough to deduce
    this from.

---
+++ tests/manual/000_missing_flush_pmemcheck.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <immintrin.h>
+
+#include <valgrind/pmemcheck.h>
+
+/**
+ */
+
+void correct(char *arr) {
+	*arr = 'c';
+	_mm_clwb(arr);
+	_mm_sfence();
+}
+
+void incorrect(char *arr) {
+	*arr = 'i';
+	// _mm_clwb(arr);
+	_mm_sfence();
+}
+
+int main(int argc, char *argv[]) {
+	char arr[100];
+	VALGRIND_PMC_REGISTER_PMEM_MAPPING(arr, sizeof(arr));
+
+	printf("Starting testing...\n");
+
+	correct(&arr[0]);
+	incorrect(&arr[64]);
+
+	printf("Test complete!\n");
+
+	VALGRIND_PMC_REMOVE_PMEM_MAPPING(arr, sizeof (arr));
+	
+	return 0;
+}
