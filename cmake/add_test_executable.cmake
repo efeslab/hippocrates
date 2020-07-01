e9c5c169d45064d866dbd4f9696754d5fe60a058
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-07-01 17:30:54 -0400

    Add custom targets for dependencies

--- cmake/add_test_executable.cmake
+++ cmake/add_test_executable.cmake
@@ -9,14 +9,14 @@ function(add_test_executable)
 
     # Now that we know that our compiler is in order, we can parse args.
     set(options)                                                                   
-    set(oneValueArgs TARGET TOOL)                                                       
-    set(multiValueArgs SOURCES EXTRA_LIBS INCLUDE)                                         
+    set(oneValueArgs TARGET)                                                       
+    set(multiValueArgs SOURCES EXTRA_LIBS INCLUDE TOOLS)                                         
     cmake_parse_arguments(FN_ARGS "${options}" "${oneValueArgs}"                   
                         "${multiValueArgs}" ${ARGN})
     
     # -- We want at least to know what tool and target, etc
-    if (NOT DEFINED FN_ARGS_TOOL)
-        message(FATAL_ERROR "Must provide TOOL argument!")
+    if (NOT DEFINED FN_ARGS_TOOLS)
+        message(FATAL_ERROR "Must provide TOOLS argument!")
     endif()
     
     add_executable(${FN_ARGS_TARGET} ${FN_ARGS_SOURCES})
@@ -24,6 +24,14 @@ function(add_test_executable)
     target_link_libraries(${FN_ARGS_TARGET} ${FN_ARGS_EXTRA_LIBS})
     # Turning off optimizations is important to avoid line-combining.
     target_compile_options(${FN_ARGS_TARGET} PUBLIC "-g;-march=native;-O0")
+
+    foreach(TOOL IN LISTS FN_ARGS_TOOLS)
+        if (TARGET ${TOOL})
+            add_dependencies(${FN_ARGS_TARGET} ${TOOL})
+        else()
+            message(WARNING "\t${TOOL} not a target!")
+        endif()
+    endforeach()
     
     add_custom_command(TARGET ${FN_ARGS_TARGET}
                        POST_BUILD
